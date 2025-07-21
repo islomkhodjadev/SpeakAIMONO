77f5b9185c598903da92c12b716cd8975c1e5d28
islomkhodja <islomkhodjadev@gmail.com>
2025-07-21 21:03:45 +0500

    good good good

--- SpeakoAIfront/speakoai/src/utils/telegram.js
+++ SpeakoAIfront/speakoai/src/utils/telegram.js
@@ -1,31 +1,31 @@
 // Utility to get tg_id from Telegram WebApp JS API
 export function getTelegramId() {
-  // if (
-  //   window.Telegram &&
-  //   window.Telegram.WebApp &&
-  //   window.Telegram.WebApp.initDataUnsafe &&
-  //   window.Telegram.WebApp.initDataUnsafe.user
-  // ) {
-  //   return window.Telegram.WebApp.initDataUnsafe.user.id;
-  // }
+  if (
+    window.Telegram &&
+    window.Telegram.WebApp &&
+    window.Telegram.WebApp.initDataUnsafe &&
+    window.Telegram.WebApp.initDataUnsafe.user
+  ) {
+    return window.Telegram.WebApp.initDataUnsafe.user.id;
+  }
   return "125364";
 }
 
 // Utility to get full Telegram user info
 export function getTelegramUser() {
-  // if (
-  //   window.Telegram &&
-  //   window.Telegram.WebApp &&
-  //   window.Telegram.WebApp.initDataUnsafe &&
-  //   window.Telegram.WebApp.initDataUnsafe.user
-  // ) {
-  //   const user = window.Telegram.WebApp.initDataUnsafe.user;
-  //   return {
-  //     tg_id: user.id,
-  //     first_name: user.first_name,
-  //     username: user.username || "string",
-  //   };
-  // }
+  if (
+    window.Telegram &&
+    window.Telegram.WebApp &&
+    window.Telegram.WebApp.initDataUnsafe &&
+    window.Telegram.WebApp.initDataUnsafe.user
+  ) {
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
+    return {
+      tg_id: user.id,
+      first_name: user.first_name,
+      username: user.username || "string",
+    };
+  }
   // fallback for dev
   return {
     tg_id: "125364",
